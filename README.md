da6b7efc5882fe87c1d8dcc19a920b6294451615
Matias Fuentes <matiasf.cortez@gmail.com>
2021-04-03 23:08:43 -0400

    full API rest

---
+++ README.md
@@ -0,0 +1,14 @@
+# DESCRIPTION
+
+A simple API rest, made with node js and mongodb.
+This API it was created as a study of nodejs.
+
+### DEPLOY
+
+you can see the API in this link:
+
+https://serverless-leocaprile.vercel.app/
+
+
+
+
---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const orders = require('./routes/orders')
+const meals = require('./routes/meals')
+
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true, useUnifiedTopology:true})
+
+
+app.get('/',(req,res)=> {
+    res.send('ruta raiz')
+})
+
+app.use(express.json())
+
+app.use('/api/orders',orders)
+app.use('/api/meals',meals)
+
+module.exports = app;
