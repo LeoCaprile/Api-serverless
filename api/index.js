f1360848defe937d1df21e9888600f5a16acefcb
Matias Fuentes <matiasf.cortez@gmail.com>
2021-04-04 21:49:49 -0400

    cors added

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const app = express();
 const orders = require('./routes/orders')
 const meals = require('./routes/meals')
 
 mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true, useUnifiedTopology:true})
 
+app.use(cors())
 
 app.get('/',(req,res)=> {
     res.send('ruta raiz')
